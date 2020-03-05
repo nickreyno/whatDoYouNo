7cc5218a3ee56d2f76ead74387d15fa01c7481c4
ChangmoSung <changmosung0914@gmail.com>
2020-03-05 18:52:41 -0500

    changed the order to call functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component, Fragment } from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 import Dictionary from "./Components/Dictionary.js";
 import LeaderBoard from "./Components/LeaderBoard.js";
 import Question from "./Components/Question.js";
@@ -6,7 +11,7 @@ import Results from "./Components/Results.js";
 import StartScreen from "./Components/StartScreen.js";
 import "./App.css";
 import axios from 'axios'
-import library from './Components/library'
+import library from './Components/library';
 import { easyWords, mediumWords, hardWords } from './Components/library'
 
 class App extends Component {
@@ -19,33 +24,46 @@ constructor(){
 }
 
 componentDidMount(){
+	this.findRandomWord(easyWords)
+}
+
+
+getComparison = (chosenWord) => {
 	axios({
 		url: `http://api.datamuse.com/words?`,
 		method: 'GET',
 		params: {
-			rel_hom: 'selectedWordFromLocalArray',
+			rel_hom: chosenWord,
 			md: 'd',
 
 		}
-	}).then(response =>{
-		console.log(response);
+	}).then(response => {
+		response = response.data;
+		const wordsWithDefs = response.filter(word => {
+			return word.defs
+		})
+		
+		console.log(response)
 	})
 }
 
 // populate our call with a word from our local array
 
-findRandomWord = () =>{
-	for (let i = easyWords.length - 1; i > 0; i--) {
-        const getRandomNumber = Math.floor(Math.random() * (i + 1));
-        [easyWords[i], easyWords[getRandomNumber]] = [easyWords[getRandomNumber], easyWords[i]];
-	};
+findRandomWord = (arrayToRandom) =>{
+	const gameArray = [];
 	
-	const selectedWordFromLocalArray = easyWords[0][0];
-	console.log(selectedWordFromLocalArray);
+	for(let i = 1; i < 10; i++) {
+		const randomNumber = Math.floor(Math.random() * arrayToRandom.length);
+		
+		const randomWord = arrayToRandom[randomNumber][0];
+
+		gameArray.push(randomWord)
+	}
 	
+	this.setState({
+		words: gameArray
+	})
 }
-
-
 
 
 
@@ -62,7 +80,7 @@ findRandomWord = () =>{
 					<header>
 						<h1>What Do You No</h1>
 						<h2>expand your vocabulary with homophones</h2>
-						<button onClick={this.findRandomWord}>start game</button>
+						<button onClick={() => this.findRandomWord(easyWords)}>start game</button>
 					</header>
 				<main>
 					<Question/>
