d2141dc4a969697e0eecaa9618cbb0f09ffeee0c
Audra T <audraturuta@gmail.com>
2020-03-09 16:37:07 -0400

    main page text and instructions

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ class App extends Component {
 			timer: "",
 			entries: ["apples", "bleak"],
 			levelSelected: false,
-			leaderBInfo: []
+			leaderBInfo: [],
+			visible: false,
 		};
 	}
 
@@ -49,6 +50,20 @@ class App extends Component {
 				leaderBInfo: stateToBeSet,
 			})
 		})
+	}
+
+	// toggle leaderboard
+
+	handleMouseDown(e){
+		this.toggleLeaderB();
+		console.log("clicked");
+		e.stopPropagation();
+	}
+
+	toggleLeaderB(){
+		this.setState({
+			visible: !this.state.visible
+		});
 	}
 
 	// populate our call with a word from our local array
@@ -109,6 +124,11 @@ class App extends Component {
 
 							<p className="homophoneDef">A <span className="homophoneItalic">homophone</span> is one of two or more words that are pronounced the same, but are different in meaning. <span className="homophoneItalic">Two, to</span> and <span className="homophoneItalic">too</span> are homophones, along with <span className="homophoneItalic">presents</span> and <span className="homophoneItalic">presence</span>.</p>
 
+							<h2 className="instructions">Instructions</h2>
+
+							<p className="homophoneDef">Select your level of difficulty below and click start. Click on the word that matches the definition shown. Your number of correct answers and time determine your place on the leaderboard! Good luck!</p>
+							
+
 							<div className="buttonContainer">
 								<button onClick={() => this.randomizer(easyWords)}>easy</button>
 								<button onClick={() => this.randomizer(mediumWords)}>medium</button>
@@ -124,9 +144,6 @@ class App extends Component {
 					</header>
 
 					<main>
-						
-						<LeaderBoard leaderBInfo={this.state.leaderBInfo} />
-						
 
 						<Route path="/questions">
 							<Question words={this.state.words} triggerResults={this.displayResults} />
@@ -147,6 +164,10 @@ class App extends Component {
 							<Dictionary entries={this.state.entries} />
 						</Route>
 					</main>
+
+					<aside>
+						<LeaderBoard leaderBInfo={this.state.leaderBInfo} />
+					</aside>
 					
 					<footer></footer>
 				</div>
