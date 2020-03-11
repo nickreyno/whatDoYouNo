d0916d21c7adc6a1456bf2b4ec8eac27ef6180fb
Nick Reyno <nickrenoe@gmail.com>
2020-03-11 10:43:04 -0400

    Updates

---
+++ precache-manifest.2a6358062fe0400edea8097ba1bdb25e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef93d8af9d74973bc5020d4e5a7a8693",
+    "url": "/whatDoYouNo/index.html"
+  },
+  {
+    "revision": "e1e0af9a38eabf8303c1",
+    "url": "/whatDoYouNo/static/css/main.3eb0b457.chunk.css"
+  },
+  {
+    "revision": "470eb2b606fb32dc2d8a",
+    "url": "/whatDoYouNo/static/js/2.ed948360.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/whatDoYouNo/static/js/2.ed948360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e0af9a38eabf8303c1",
+    "url": "/whatDoYouNo/static/js/main.fe758e98.chunk.js"
+  },
+  {
+    "revision": "19e8214eea06fa0fcabc",
+    "url": "/whatDoYouNo/static/js/runtime-main.8933c4c1.js"
+  },
+  {
+    "revision": "9f3db4558b07cfb6ed92417983257960",
+    "url": "/whatDoYouNo/static/media/knowledge.9f3db455.webp"
+  }
+]);
